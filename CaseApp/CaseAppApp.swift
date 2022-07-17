c6e9cf172b035b8f8dc71d6db06d841e32095cbd
Emir Alkal <alkalemir@gmail.com>
2022-07-17 18:55:22 +0300

    Initial Commit

---
+++ CaseApp/CaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseAppApp.swift
+//  CaseApp
+//
+//  Created by Emir Alkal on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaseApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CaseApp
+//
+//  Created by Emir Alkal on 17.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
